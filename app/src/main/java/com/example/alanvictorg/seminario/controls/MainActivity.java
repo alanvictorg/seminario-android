28ab8f128d1e37fc5cbc6a15242a11775aee862f
alanvictorg <alanvictorgp@gmail.com>
2017-09-14 00:27:11 -0300

    Inicio menu

--- app/src/main/java/com/example/alanvictorg/seminario/controls/MainActivity.java
+++ app/src/main/java/com/example/alanvictorg/seminario/controls/MainActivity.java
@@ -1,32 +1,42 @@
 package com.example.alanvictorg.seminario.controls;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.alanvictorg.seminario.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtNome;
-    TextView txtEmail;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtNome = (TextView) findViewById(R.id.txtPrincipalNome);
-        txtEmail = (TextView) findViewById(R.id.txtPrincipalEmail);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
 
-        Intent it = getIntent();
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
 
-        String nome = it.getStringExtra("nome");
-        txtNome.setText(nome);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
-        String email = it.getStringExtra("email");
-        txtEmail.setText(email);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
